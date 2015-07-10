afe4d276fc2093810c11a28011f574e796abd7b2
Emmanuel Marcha <mmarcha@opinionlab.com>
2015-07-10 15:55:34 -0500

    Initialize project

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ccPaydownVizApp
+ * @description
+ * # ccPaydownVizApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ccPaydownVizApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ccPaydownVizApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ccPaydownVizApp
+ */
+angular.module('ccPaydownVizApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
